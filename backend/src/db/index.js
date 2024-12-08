da27218a9948da41cbdd66128776b591d505687f
sanaf3739 <sanaf6350@gmail.com>
2024-12-08 22:00:12 +0530

    connected mongo db atlas

---
+++ backend/src/db/index.js
@@ -0,0 +1,15 @@
+import { DB_NAME } from "../constants.js";
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+    try{
+      const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+      console.log(`\n MongoDB Connected !! DB HOST: ${connectionInstance.connection.host}`)
+    }
+    catch(error){
+        console.log("MONGODB Connection Error", error);
+        process.exit(1)
+    }
+}
+
+export default connectDb;
---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+// require('dotenv').config({path: "./env"})
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./env",
+});
+import connectDb from "./db/index.js";
+
+connectDb();
+
+// First approach to connect DB
+/*
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.listen("error", (error) => {
+        console.log("ERROR", error);
+        throw error
+    })
+    app.listen(process.env.PORT, ()=> {
+        console.log(`App is listening on port ${process.env.PORT}`)
+    })
+  } catch (error) {
+    console.log("ERROR", error);
+    throw error
+  }
+})();*/
